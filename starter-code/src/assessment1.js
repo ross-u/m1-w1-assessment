6295936a370a7c8a4cdc32fa66c56f908a193923
Stephen Coley <stephencoley@Stephens-MacBook-Air.local>
2019-10-18 20:51:25 +0200

    gitPractice

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -36,33 +36,48 @@ function maxOfThreeNumbers(a, b, c) {
 
 // Calculate the sum of an array of numbers
 function sumArray(numbers) {
-  return sumArray;
+  return numbers.reduce(function(total, cur) {
+    return total + cur;
+  }, 0);
 }
 
 // Return the biggest number of a non-empty array
+
 function maxOfArray(numbers) {
-  //
+  let res = Math.max(...numbers);
+  console.log(res);
+  return res;
 }
 
 // Return the longest string in an array
 function longestString(strings) {
-  //
+  return strings.reduce(function(total, cur) {
+    if (total.length >= cur.length) return total;
+    return cur;
+  }, "");
 }
 
 // Return whether a word is in an array
 function doesWordExist(wordsArr, word) {
-  //
+  return wordsArr.includes(word);
 }
+//
 
 // Finding the first non-duplicate word in an array
 function findUnique(wordsArr) {
-  //
+  if (!wordsArr.length) return false;
+  return wordsArr.find(function(el) {
+    return wordsArr.indexOf(el) === wordsArr.lastIndexOf(el);
+  });
 }
 
 // Get the fullName from the object { firstName: 'Tony', lastName: 'Stark'}
-function getFullName(personObj) {}
+function getFullName(personObj) {
+  return `${personObj.firstName} ${personObj.lastName}`;
+}
 
 // Return the biggest number in a two dimensional array
 function maxTwoDimArray(matrix) {
-  //
+  return Math.max(...matrix.map(array => Math.max(...array)));
 }
+console.log("hi recap");
