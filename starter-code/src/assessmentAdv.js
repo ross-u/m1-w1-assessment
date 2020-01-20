25346484b29bab8a91252a23a3b32fde3a7ce328
Jethet <henriette.hettinga@gmail.com>
2020-01-20 21:27:30 +0100

    Adv assessment tests

--- starter-code/src/assessmentAdv.js
+++ starter-code/src/assessmentAdv.js
@@ -13,3 +13,32 @@ function isEven(num) {
     }
     return true;
 }
+
+function maxOfTwoNumbers(a, b) {
+    if (a < 0 && b < 0){
+      if (a > b){
+        return a;
+      } else if (b > a){
+        return b;
+        }
+    } 
+    if (a > b) {
+        return a;
+     } else {
+        return b;
+    }
+}
+
+function maxOfThreeNumbers(a, b, c) {
+    if (isNaN(a) || isNaN(b) || isNaN(c)){
+      return false
+    } else if (a > b && a > c){
+      return a;
+    } else if (b > a && b > c ) {
+      return b;
+    } else {
+      return c;
+    }
+  }
+
+  
