4c05c546c6f85134cc226f0567cfc08c64c4364a
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-01-20 10:45:44 +0100

    8 left

--- starter-code/src/assessment1.js
+++ starter-code/src/assessment1.js
@@ -1,27 +1,68 @@
 // Write a function that returns the product of 2 numbers
 function product(x, y) {
+  return x * y;
   //
 }
 
 // Write a function that returns whether a given number is even
 function isEven(num) {
+  if (num % 2 === 0 ) {
+    return true
+  }
+  else {
+    return false}
   //
 }
 
 // Return the largest of 2 numbers
 function maxOfTwoNumbers(a, b) {
+  if (a > b) {
+    return a;
+  }
+    else {
+      return b;
+    }
+  }
+
   //
-}
 
 // Return the largest of 3 numbers
 function maxOfThreeNumbers(a, b, c) {
   //
+  if (a > b && a > c) {
+    return a;
+  }
+  else if (b > a && b > c) {
+    return b;
+  }
+  else if (c > a && c > b) {
+    return c;
+  }
 }
 
 // Calculate the sum of an array of numbers
+
+const numsArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//OPTION 1 with reduce
+// function sumArray(numbers) {
+//   return numbers.reduce(function (accumulator, currentValue){
+//       const updatedAccumulator = accumulator + currentValue;
+//           return updatedAccumulator;
+//     }, 0);
+// }
+
+//OPTION 2 with loop
+var total = 0;
 function sumArray(numbers) {
-  //
+  var total = 0;
+  for(var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+  }
+
+  return total;
 }
+
+
 
 // Return the largest number of a non-empty array
 function maxOfArray(numbers) {
